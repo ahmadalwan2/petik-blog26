28d235a4369e18903d7b7cc725eb4371e095eb86
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 09:36:04 +0700

    Membuat file looping.js dan menambahkan isi datanya

---
+++ looping.js
@@ -0,0 +1,11 @@
+ // === Membuat LOOPING YANG SEDERHANA DIDALAM JAVASCRIPT === //
+
+ // - Looping for:
+ // nilai awal
+ // nilai akhir/kondisi akhir
+ // increment/decrement 
+
+ for (let i = 0; i < 10; i++) {
+        console.log(`Looping ke-${i}`);
+            
+ }
